7edc047949937de493c1d114c696e428deb7af53
c0het <c0het401@gmail.com>
2022-05-22 15:09:12 +0300

    7. ListIterator [#350217 #302792]

--- src/main/java/ru/job4j/set/SimpleSet.java
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -10,30 +10,6 @@ public class SimpleSet<T> implements Set<T> {
     private SimpleArrayList<T> set = new SimpleArrayList<>(10);
 
     @Override
-    public String toString() {
-        return "SimpleSet{"
-                + "set=" + set
-                +  '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof SimpleSet)) {
-            return false;
-        }
-        SimpleSet<?> simpleSet = (SimpleSet<?>) o;
-        return set.equals(simpleSet.set);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(set);
-    }
-
-    @Override
     public boolean add(T value) {
         boolean rsl = set.size() == 0 || !contains(value);
         if (rsl) {
@@ -46,7 +22,7 @@ public class SimpleSet<T> implements Set<T> {
     public boolean contains(T value) {
         boolean rsl = false;
         for (T s: set) {
-            if (s == null && value == null || value.equals(s)) {
+            if (Objects.equals(s, value)) {
                 rsl = true;
                 break;
             }
