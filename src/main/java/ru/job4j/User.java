d86f81356c0e9076cc6aa522bef3d88ba6e93fbe
c0het <c0het401@gmail.com>
2022-05-25 22:51:12 +0300

    1. Создать модель User [#999]

--- src/main/java/ru/job4j/User.java
+++ src/main/java/ru/job4j/User.java
@@ -1,6 +1,6 @@
 package ru.job4j;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
 
@@ -8,9 +8,44 @@ public class User {
     private int children;
     private Calendar birthday;
 
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday.getTime()
+                + '}';
+    }
+
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Nick", 1, new GregorianCalendar(2000, Calendar.JANUARY, 10));
+        User user2 = new User("Nick", 1, new GregorianCalendar(2000, Calendar.JANUARY, 10));
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        System.out.println(map);
     }
 }
