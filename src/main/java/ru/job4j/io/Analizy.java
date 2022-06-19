c22d5218f55bf856aea24bce707cf9af6ce3424d
c0het <c0het401@gmail.com>
2022-06-19 19:10:53 +0300

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,34 +1,34 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        StringJoiner in = new StringJoiner(System.lineSeparator());
-        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            read.lines().forEach(in::add);
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(target));
+             BufferedReader readBuf = new BufferedReader(new FileReader(source))) {
+            boolean flag400or500 = false;
+            boolean flag200or300 = false;
+            for (String read
+                    : readBuf.lines()
+                    .collect(Collectors.joining(System.lineSeparator()))
+                    .split("\r\n")) {
+                if (flag400or500 && Integer.parseInt(read.split(" ")[0]) == 400
+                        || Integer.parseInt(read.split(" ")[0]) == 500) {
+                    flag200or300 = true;
+                    out.print(read.substring(3));
+                } else if (flag200or300 && Integer.parseInt(read.split(" ")[0]) == 200
+                        || Integer.parseInt(read.split(" ")[0]) == 300) {
+                    flag400or500 = true;
+                    out.println(" -" + read.substring(3));
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-        boolean flag400or500 = false;
-        boolean flag200or300 = false;
-        for (String read : in.toString().split("\r\n")) {
-            if (flag400or500 && Integer.parseInt(read.split(" ")[0]) == 400
-                    || Integer.parseInt(read.split(" ")[0]) == 500) {
-                flag200or300 = true;
-                out.print(read.substring(3));
-            } else if (flag200or300 && Integer.parseInt(read.split(" ")[0]) == 200
-                        || Integer.parseInt(read.split(" ")[0]) == 300) {
-                flag400or500 = true;
-                out.println(" -" + read.substring(3));
-                    }
-                }
-            } catch (IOException e) {
-            e.printStackTrace();
-        }
-        }
+    }
+
+
 
     public static void main(String[] args) {
         Analizy analizy = new Analizy();
