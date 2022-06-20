7fb0d51c1aaa417e24e0dc7953b3c71db711b896
c0het <c0het401@gmail.com>
2022-06-20 22:06:02 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -12,14 +12,14 @@ import static org.hamcrest.Matchers.is;
 
 public class AnalizyTest {
     @Rule
-    public TemporaryFolder folder = new TemporaryFolder();
+    public TemporaryFolder folder = new TemporaryFolder() {
 
-    @Test
-    public void test() throws IOException {
-        File source = folder.newFile("source.txt");
-        File target = folder.newFile("target.txt");
-        try (PrintWriter out = new PrintWriter(source)) {
-            out.println("""
+        @Test
+        public void test() throws IOException {
+            File source = folder.newFile("source.txt");
+            File target = folder.newFile("target.txt");
+            try (PrintWriter out = new PrintWriter(source)) {
+                out.println("""
                         200 10:56:01
                         500 10:57:01
                         400 10:58:01
@@ -28,12 +28,13 @@ public class AnalizyTest {
                         500 11:01:02
                         200 11:02:02""");
             }
-        Analizy analizy = new Analizy();
-        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
-        StringBuilder rsl = new StringBuilder();
-        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-            in.lines().forEach(rsl::append);
+            Analizy analizy = new Analizy();
+            analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+            StringBuilder rsl = new StringBuilder();
+            try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+                in.lines().forEach(rsl::append);
+            }
+            assertThat(rsl.toString(), is(" 10:57:01 - 10:59:01 11:01:02 - 11:02:02"));
         }
-        assertThat(rsl.toString(), is(" 10:57:01 - 10:59:01 11:01:02 - 11:02:02"));
-    }
+    };
 }
