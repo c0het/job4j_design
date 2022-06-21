9b3489ffc4761bf4be5cd7a8b47f816e26d61def
c0het <c0het401@gmail.com>
2022-06-21 08:28:05 +0300

    3.0. Тестирование IO [#173905]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -12,23 +12,23 @@ import static org.hamcrest.Matchers.is;
 
 public class AnalizyTest {
     @Rule
-    public TemporaryFolder folder = new TemporaryFolder() {
+    public TemporaryFolder folder = new TemporaryFolder();
 
-        @Test
-        public void test() throws IOException {
-            File source = folder.newFile("source.txt");
-            File target = folder.newFile("target.txt");
-            try (PrintWriter out = new PrintWriter(source)) {
-                out.println("200 10:56:01\n" + "500 10:57:01\n" + "400 10:58:01\n" + "200 10:59:01\n"
-                        + "300 10:59:55\n" + "500 11:01:02\n" + " 200 11:02:02");
-            }
-            Analizy analizy = new Analizy();
-            analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
-            StringBuilder rsl = new StringBuilder();
-            try (BufferedReader in = new BufferedReader(new FileReader(target))) {
-                in.lines().forEach(rsl::append);
-            }
-            assertThat(rsl.toString(), is(" 10:57:01 - 10:59:01 11:01:02 - 11:02:02"));
+    @Test
+    public void test() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01\n" + "500 10:57:01\n" + "400 10:58:01\n" + "200 10:59:01\n"
+                    + "300 10:59:55\n" + "500 11:01:02\n" + "200 11:02:02");
         }
-    };
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        assertThat(rsl.toString(), is(" 10:57:01 - 10:59:01 11:01:02 - 11:02:02"));
+    }
 }
+
