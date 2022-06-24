985ec1c1041e33b08dd3c1afc751321f8453383c
c0het <c0het401@gmail.com>
2022-06-24 07:35:35 +0300

    4.2. Поиск дубликатов [#315066 #315451]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -1,24 +1,33 @@
 package ru.job4j.io.duplicates;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
         DuplicatesVisitor duplicatesVisitor = new DuplicatesVisitor();
         Files.walkFileTree(Path.of("."), duplicatesVisitor);
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("DuplicatesVisitor.jar"))) {
-            for (FileProperty key : duplicatesVisitor.registry.keySet()) {
-                if (duplicatesVisitor.registry.get(key).size() != 1) {
+        DuplicatesFinder finder = new DuplicatesFinder();
+        finder.duplicatesPrint(new File("DuplicatesVisitor.jar"), duplicatesVisitor.registry);
+
+    }
+
+    public void duplicatesPrint(File file, HashMap<FileProperty, List<Path>> paths) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream(file))) {
+            for (FileProperty key : paths.keySet()) {
+                if (paths.get(key).size() != 1) {
                     System.out.println(key.getName() + " - " + key.getSize() + "b");
                     out.println(key.getName() + " - " + key.getSize() + "b");
-                    for (Path paths: duplicatesVisitor.registry.get(key)) {
-                        System.out.println(paths.normalize().toFile().getAbsolutePath());
-                        out.println(paths.normalize().toFile().getAbsolutePath());
+                    for (Path link : paths.get(key)) {
+                        System.out.println(link.normalize().toFile().getAbsolutePath());
+                        out.println(link.normalize().toFile().getAbsolutePath());
                     }
                 }
             }
