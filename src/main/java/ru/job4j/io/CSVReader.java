89342bd78f279bff6e21ff49904f04ca089db94e
c0het <c0het401@gmail.com>
2022-07-12 20:40:15 +0300

    7. Scanner [#504791]

---
+++ src/main/java/ru/job4j/io/CSVReader.java
@@ -0,0 +1,37 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.*;
+
+public class CSVReader {
+    public static void handle(ArgsName argsName) throws Exception {
+        List<List<String>> lines = new ArrayList<>();
+        List<Integer> index = new ArrayList<>();
+        List<String> rsl = new ArrayList<>();
+        try (Scanner scanner = new Scanner(new File(argsName.get("path")));
+             PrintWriter out = new PrintWriter(new FileOutputStream(argsName.get("out")))) {
+            while (scanner.hasNext()) {
+                List<String> line = new ArrayList<>(Arrays.asList(scanner.nextLine().split(argsName.get("delimiter"))));
+                lines.add(line);
+            }
+            for (String line : lines.get(0)) {
+                for (String filter : argsName.get("filter").split(",")) {
+                    if (line.equals(filter)) {
+                        index.add(lines.get(0).indexOf(line));
+                    }
+                }
+            }
+            for (List<String> line : lines) {
+                    for (int i : index) {
+                        if (index.indexOf(i) != index.size() - 1) {
+                            out.print(line.get(i) + ";");
+                    } else {
+                            out.print(line.get(i) + System.lineSeparator());
+                        }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
