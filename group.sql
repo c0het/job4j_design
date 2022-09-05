97165f49073650a29f2a00cbc97985d1e5565d0e
c0het <c0het401@gmail.com>
2022-09-05 23:23:30 +0300

    2. Группировка и агрегатные функции [#373618]

---
+++ group.sql
@@ -0,0 +1,29 @@
+create table devices(
+    id serial primary key,
+    name varchar(255),
+    price float
+);
+
+create table people(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table devices_people(
+    id serial primary key,
+    device_id int references devices(id),
+    people_id int references people(id)
+);
+
+INSERT INTO devices(name, price) VALUES ('Poco', 4999), ('Apple', 80000), ('Samsung', 30000);
+INSERT INTO people(name) VALUES('Nikolai'),('Dmitrii'),('Sergey');
+INSERT INTO devices_people(device_id,people_id) VALUES (1,2),(1,3),(2,1),(3,2);
+
+SELECT AVG(d.price) FROM devices AS d;
+
+SELECT dp.people_id, AVG(d.price) AS "Средняя цена"
+FROM devices_people AS dp 
+JOIN devices AS d
+ON dp.device_id = d.id
+GROUP BY dp.people_id
+HAVING AVG(d.price) > 5000;
