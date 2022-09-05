fb37157a1f655ae2919f46f99fb3048a6d3ab53a
c0het <c0het401@gmail.com>
2022-09-05 08:31:12 +0300

    1. Inner join [#373617]

--- innerJoin.sql
+++ innerJoin.sql
@@ -1,6 +1,7 @@
 CREATE TABLE book(
 	id serial PRIMARY KEY,
-	name varchar(255)
+	name varchar(255),
+	year int
 );
 
 
@@ -10,9 +11,9 @@ CREATE TABLE author (
 	book_id int references book(id)
 );
 
-INSERT into book(name) VALUES ('War and Peace');
-INSERT into book(name) VALUES ('Harry Potter');
-INSERT into book(name) VALUES ('To Kill a Mockingbird');
+INSERT into book(name, year) VALUES ('War and Peace', 1867);
+INSERT into book(name, year) VALUES ('Harry Potter', 1997);
+INSERT into book(name, year) VALUES ('To Kill a Mockingbird', 1960);
 
 INSERT INTO author(name, book_id) VALUES ('Lev Tolstoi', 1);
 INSERT INTO author(name, book_id) VALUES ('Harper Lee', 3);
@@ -20,5 +21,11 @@ INSERT INTO author(name, book_id) VALUES ('Joanne Rowling', 2);
 
 INSERT INTO author(name) VALUES ('Pushkin');
 
-SELECT b.name AS "Книга", a.name AS "Автор"
+SELECT b.name AS "Книга", a.name AS "Автор" , b.year AS "Первая публикация"
+FROM book b JOIN author a ON a.book_id = b.id;
+
+SELECT b.name, a.name INNER
+FROM book AS b JOIN author AS a ON a.book_id = b.id;
+
+SELECT b.name, a.name 
 FROM book b JOIN author a ON a.book_id = b.id;
