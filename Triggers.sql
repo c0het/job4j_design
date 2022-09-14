fab555a36cf6b45cb3f0ef8522e9a88d937326a4
c0het <c0het401@gmail.com>
2022-09-14 21:48:06 +0300

    2. Триггеры [#504803 #343094]

--- Triggers.sql
+++ Triggers.sql
@@ -9,43 +9,42 @@ create table productss (
 INSERT INTO productss(name, producer, count, price) VALUES ('product1', 'producer1', 5, 10);
 INSERT INTO productss(name, producer, count, price) VALUES ('product2', 'producer2', 4, 20);
 
+CREATE FUNCTION tax_insert()
+	RETURNS TRIGGER AS
+$$
+	BEGIN
+		UPDATE productss
+		SET price = price + price * 0.18
+		WHERE id = (SELECT ID FROM inserted );
+		RETURN NEW;
+	END;
+$$
+LANGUAGE 'plpgsql';
 
-
-CREATE TRIGGER tax_plus
+CREATE OR REPLACE TRIGGER tax_for_insert
 	AFTER INSERT ON productss
 	REFERENCING NEW TABLE AS inserted 
 	FOR EACH STATEMENT
-	EXECUTE PROCEDURE tax();
+	EXECUTE PROCEDURE tax_insert();
 	
-CREATE OR REPLACE FUNCTION tax()
+
+CREATE OR REPLACE FUNCTION tax_last()
 	RETURNS TRIGGER AS
 $$
 	BEGIN
-		UPDATE productss
-		SET price = price + price * 0.18;
+		NEW.price = NEW.price + NEW.price * 0.18;
 		RETURN NEW;
 	END;
 $$
 LANGUAGE 'plpgsql';
 
 
-
-CREATE TRIGGER tax_plus
+CREATE TRIGGER tax_plus_last
 	BEFORE INSERT
 	ON productss
 	FOR EACH ROW
-	EXECUTE PROCEDURE tax();
+	EXECUTE PROCEDURE tax_last();
 	
-CREATE OR REPLACE FUNCTION tax()
-	RETURNS TRIGGER AS
-$$
-	BEGIN
-		UPDATE productss
-		SET price = price + price * 0.18;
-		RETURN NEW;
-	END;
-$$
-LANGUAGE 'plpgsql';
 
 
 
@@ -55,13 +54,6 @@ create table history_of_price (
     price integer,
     date timestamp
 );
-
-CREATE TRIGGER add_in_history
-	BEFORE INSERT
-	ON productss
-	FOR EACH ROW
-	EXECUTE PROCEDURE add_in_history();
-
 
 CREATE FUNCTION add_in_history()
 	RETURNS TRIGGER AS
@@ -73,6 +65,18 @@ $$
 	END;
 $$
 LANGUAGE 'plpgsql'
+
+CREATE TRIGGER add_in_history
+	BEFORE INSERT
+	ON productss
+	FOR EACH ROW
+	EXECUTE PROCEDURE add_in_history();
+
+
+
+
+SELECT *
+FROM productss
 
 SELECT *
 FROM history_of_price
