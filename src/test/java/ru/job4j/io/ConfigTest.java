b6b94c0094084ae180774442daa2d16bcab8691a
c0het <c0het401@gmail.com>
2022-09-19 21:48:28 +0300

    0. JDBC [#6863]

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -24,7 +24,7 @@ public class ConfigTest {
         config.load();
         assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
         assertThat(config.value("hibernate.connection.url"),
-                is("jdbc:postgresql://127.0.0.1:5432/idea_db"));
+                is("jdbc:postgresql://localhost:5432/idea_db"));
         assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
         assertThat(config.value("hibernate.connection.username"), is("postgres"));
         assertThat(config.value("hibernate.connection.password"), is("password"));
