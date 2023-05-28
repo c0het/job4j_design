2e905cda7da6f868250937ac6b7b625a0e6902e5
c0het <c0het401@gmail.com>
2023-05-28 17:08:51 +0300

    5. Утверждения с коллекциями [#504885]

---
+++ src/test/java/ru/job4j/assertj/SimpleConvertTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.assertj;
+
+import org.assertj.core.data.Index;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleConvertTest {
+    @Test
+    void checkArray() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        String[] array = simpleConvert.toArray("first", "second", "three", "four", "five");
+        assertThat(array).hasSize(5)
+                .contains("second")
+                .contains("first", Index.atIndex(0))
+                .containsAnyOf("zero", "second", "six")
+                .doesNotContain("first", Index.atIndex(1));
+    }
+
+    @Test
+    void checkList() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        List<String> list = simpleConvert.toList("first", "second", "three", "four", "five");
+        assertThat(list).contains("second")
+                .isNotEmpty()
+                .hasSize(5)
+                .startsWith("first")
+                .element(0)
+                .isNotNull();
+    }
+
+    @Test
+    void checkSet() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Set<String> set = simpleConvert.toSet("first", "second", "three", "four", "five");
+        assertThat(set).contains("four", "five")
+                .hasSizeGreaterThan(3)
+                .usingDefaultComparator()
+                .filteredOnAssertions(e -> assertThat(e).isNotNull());
+    }
+
+    @Test
+    void checkMap() {
+        SimpleConvert simpleConvert = new SimpleConvert();
+        Map<String, Integer> map = simpleConvert.toMap("first", "second", "three", "four", "five");
+        assertThat(map)
+                .containsKey("first")
+                .containsValue(1)
+                .hasSizeGreaterThan(3);
+    }
+}
